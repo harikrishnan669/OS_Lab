79a822b2b60dc9105fbfb44ac2a657fc3d067ab2
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-06 21:42:34 +0530

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -2,43 +2,52 @@
 #include<pthread.h>
 #include<unistd.h>
 #include<stdlib.h>
-int sempahore=1;
-int state=1;
-void wait(int *sempahore)
-{
-    while(*sempahore<=0);
-    *sempahore--;
+
+int semaphore = 1;
+int state = 1;
+pthread_mutex_t lock;
+
+void wait(int *semaphore) {
+    while(*semaphore <= 0);
+    (*semaphore)--;
 }
-void signal(int *sempahore)
-{
-    //sleep(1);
-    *sempahore++;
+
+void signal(int *semaphore) {
+    (*semaphore)++;
 }
-void *thread_f(void *arg)
-{
-    int no=*((int*)arg);
-    int st=no;
-    while(1)
-    {
-        wait(&sempahore);
-        if(state=st)
-        {
-            printf("Thread [%d]-> %d\n",st,no);
-            no=no+2;
-            state=3-st;
+
+void *thread_f(void *arg) {
+    int no = *((int *)arg);
+    int st = no;
+    while(1) {
+        pthread_mutex_lock(&lock);
+        wait(&semaphore);
+        if(state == st) {
+            printf("Thread [%d]-> %d\n", st, no);
+            no = no + 2;
+            state = 3 - st;
         }
-        signal(&sempahore);
+        signal(&semaphore);
+        pthread_mutex_unlock(&lock);
+        usleep(100000); 
     }
     pthread_exit(NULL);
 }
-int main()
-{
-    pthread_t id1,id2;
-    int odd=1;
-    int even=2;
-    pthread_create(&id1,NULL,(void *)thread_f,&odd);
-    pthread_create(&id2,NULL,(void *)thread_f,&even);
-    pthread_join(id1,NULL);
-    pthread_join(id2,NULL);
+
+int main() {
+    pthread_t id1, id2;
+    int odd = 1;
+    int even = 2;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&id1, NULL, thread_f, (void *)&odd);
+    pthread_create(&id2, NULL, thread_f, (void *)&even);
+
+    pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
+
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
