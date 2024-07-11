bb3958d80f5cfb66236e5924aca37b731b8776c4
Harikrishnan R <128880349+harikrishnan669@users.noreply.github.com>
2024-07-11 19:06:42 +0530

    Update pagereplacement.c

--- pagereplacement.c
+++ pagereplacement.c
@@ -1,113 +1,79 @@
 #include<stdio.h>
-
-typedef struct {
-    int PageN;
-    int lastUsed;
-    int freq;
-} Frame;
-void FIFS(Frame Frames[], int Frame_Size, int Pages[], int n) {
-    printf("\n\tFIFS\n");
-    int pageFault = 0, index = -1;
-    for (int i = 0; i < Frame_Size; i++) Frames[i].PageN = -1;
-    for (int i = 0; i < n; i++) {
-        int pageFound = 0;
-        for (int j = 0; j < Frame_Size; j++) {
-            if (Frames[j].PageN == Pages[i]) {
-                pageFound = 1;
+#include<stdlib.h>
+int n,f,i,j,k,fault=0,flag;
+void printframes(int frames[],int f)
+{
+    int i;
+    for(i=0;i<f;i++)
+    {
+       if(frames[i]==-1)
+       {
+            printf("_\t");
+       }
+       else{
+       printf("%d\t",frames[i]);
+       }
+    }
+    printf("\n");
+}
+void fifo(int pages[],int n,int realframes[],int f)
+{
+    int frames[f];
+    for(j=0;j<f;j++)
+    {
+        frames[j]=realframes[j];
+    }
+    printf("FIFO\n");
+    fault=0;
+    k=0;
+    for(i=0;i<n;i++)
+    {
+        int p;
+        p=pages[i];
+        flag=0; 
+        for(j=0;j<f;j++)
+        {
+            if(p==frames[j])
+            {
+                flag=1;
                 break;
             }
         }
-        if (!pageFound) {
-            pageFault++;
-            index = (index + 1) % Frame_Size;
-            Frames[index].PageN = Pages[i];
+        if(flag==0)
+        {
+            fault++;
+            frames[k]=p;
+            k=(k+1)%f;
+            printframes(frames,f);
+        }
+        else
+        {
+            printframes(frames,f);
         }
     }
-    printf("\nNo of PageFaults = %3d ", pageFault);
-    printf("\nNo of PageHits = %3d ", n - pageFault);
+    printf("page fault=%d",fault);    
 }
 
-void LRU(Frame Frames[], int Frame_Size, int Pages[], int n) {
-    printf("\n\tLRU\n");
-    int pageFault = 0;
-    for (int i = 0; i < Frame_Size; i++) {
-        Frames[i].PageN = -1;
-        Frames[i].lastUsed = 0;
+void main()
+{
+    
+    printf("enter the no of pages");
+    scanf("%d",&n);
+    printf("enter the no of frames");
+    scanf("%d",&f);
+    int pages[n],realframes[f];
+    printf("enter page squences");
+    //printf("hiiii");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&pages[i]);
     }
-    int z = 1;
-    for (int i = 0; i < n; i++) {
-        int pageFound = 0;
-        for (int j = 0; j < Frame_Size; j++) {
-            if (Frames[j].PageN == Pages[i]) {
-                pageFound = 1;
-                Frames[j].lastUsed = z++;
-                break;
-            }
-        }
-        if (!pageFound) {
-            pageFault++;
-            int min = 0;
-            for (int j = 1; j < Frame_Size; j++) {
-                if (Frames[j].lastUsed < Frames[min].lastUsed) {
-                    min = j;
-                }
-            }
-            Frames[min].PageN = Pages[i];
-            Frames[min].lastUsed = z++;
-        }
+    for(j=0;j<f;j++)
+    {
+        realframes[j]=-1;
     }
-    printf("\nNo of PageFaults = %3d ", pageFault);
-    printf("\nNo of PageHits = %3d ", n - pageFault);
-}
+    fifo(pages,n,realframes,f);
 
-void LFU(Frame Frames[], int Frame_Size, int Pages[], int n) {
-    printf("\n\tLFU\n");
-    int pageFault = 0;
-    for (int i = 0; i < Frame_Size; i++) {
-        Frames[i].PageN = -1;
-        Frames[i].freq = 0;
-    }
-    int index = 0;
-    for (int i = 0; i < n; i++) {
-        int pageFound = 0;
-        for (int j = 0; j < Frame_Size; j++) {
-            if (Frames[j].PageN == Pages[i]) {
-                pageFound = 1;
-                Frames[j].freq++;
-                break;
-            }
-        }
-        if (!pageFound) {
-            pageFault++;
-            int min = index;
-            for (int j = 0; j < Frame_Size; j++) {
-                if (Frames[index].freq < Frames[min].freq) {
-                    min = index;
-                }
-                index = (index + 1) % Frame_Size;
-            }
-            Frames[min].PageN = Pages[i];
-            Frames[min].freq = 1;
-            index = (min + 1) % Frame_Size;
-        }
-    }
-    printf("\nNo of PageFaults = %3d ", pageFault);
-    printf("\nNo of PageHits = %3d ", n - pageFault);
-}
 
-int main() {
-    int Frame_Size, n;
-    printf("Enter the size of Frame: ");
-    scanf("%d", &Frame_Size);
-    Frame Frames[Frame_Size];
-    printf("Enter the Number of pages: ");
-    scanf("%d", &n);
-    int Pages[n];
-    printf("Enter the Page Numbers: ");
-    for (int i = 0; i < n; i++) 
-    scanf("%d", &Pages[i]);
-    FIFS(Frames, Frame_Size, Pages, n);
-    LRU(Frames, Frame_Size, Pages, n);
-    LFU(Frames, Frame_Size, Pages, n);
-    return 0;
+    
 }
